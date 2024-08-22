6a5b6cb47bd9e60f618a57355e7641cb1f5e2783
snehapriya-bs <152855542+snehapriya-bs@users.noreply.github.com>
2024-08-22 07:55:02 +0530

    Update remote-state.tf

--- terraform/remote-state.tf
+++ terraform/remote-state.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "remote" {
-    organization = "Harrisson"
+    organization = "Snehapriya"
 
     workspaces {
-      name = "teste1"
+      name = "first-project"
     }
   }
 }
